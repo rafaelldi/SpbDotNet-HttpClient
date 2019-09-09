3922a37308671b15334e439062e98f65506c73e2
rafaelldi <rival.abdrakhmanov@outlook.com>
2019-09-09 21:57:32 +0300

    dirty hack, have a better idea ?

--- src/HttpClientApp/HttpClientWebApp/ServiceWithNamedClient.cs
+++ src/HttpClientApp/HttpClientWebApp/ServiceWithNamedClient.cs
@@ -1,5 +1,7 @@
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace HttpClientWebApp
 {
@@ -17,6 +19,29 @@ namespace HttpClientWebApp
             var client = _httpClientFactory.CreateClient("some-site");
             var response = await client.GetAsync("/get-some-data");
             return response;
+        }
+
+        public async Task<SomeData> GetSomeDataFromString()
+        {
+            var client = _httpClientFactory.CreateClient("some-site");
+            var response = await client.GetAsync("/get-some-data");
+            var responseString = await response.Content.ReadAsStringAsync();
+            var someData = JsonConvert.DeserializeObject<SomeData>(responseString);
+            return someData;
+        }
+        
+        public async Task<SomeData> GetSomeDataFromStream()
+        {
+            var client = _httpClientFactory.CreateClient("some-site");
+            var response = await client.GetAsync("/get-some-data");
+            var serializer = new JsonSerializer();
+            var stream = await response.Content.ReadAsStreamAsync();
+            using (var sr = new StreamReader(stream))
+            using (var jsonReader = new JsonTextReader(sr))
+            {
+                var someData = serializer.Deserialize<SomeData>(jsonReader);
+                return someData;
+            }
         }
     }
 }
