d338fd5e1ea56d492649d357d4b8259f01cdcee3
rafaelldi <rival.abdrakhmanov@outlook.com>
2019-09-09 22:58:52 +0300

    fix tpyo

--- src/HttpClientApp/HttpClientWebApp30/Startup.cs
+++ src/HttpClientApp/HttpClientWebApp30/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -19,6 +20,10 @@ namespace HttpClientWebApp30
         {
             services.AddControllers();
             services.AddHttpClient("some-site");
+            services.AddGrpcClient<>(options =>
+            {
+                options.Address = new Uri("https://localhost:5001");
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
