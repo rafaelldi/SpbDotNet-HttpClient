248247d3f41fe478a9a77cf0744cf741f9bb3ee3
rafaelldi <rival.abdrakhmanov@outlook.com>
2019-09-28 15:55:15 +0300

    The last time I tried this the monkey didn't survive. Let's hope it works better this time.

---
+++ src/HttpClientApp/HttpClientVersion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpClientVersion
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient() { BaseAddress = new Uri("https://microsoft.com") };
+            
+            using (var request = new HttpRequestMessage(HttpMethod.Get, "/") { Version = new Version(2, 0) })
+            using (var response20 = await client.SendAsync(request))
+                Console.WriteLine($"Request version is {request.Version}\nHTTP Version is: {response20.Version}\n.NET Core Version: {System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription}");
+
+            using (var request = new HttpRequestMessage(HttpMethod.Get, "/"))
+            using (var response11 = await client.SendAsync(request))
+                Console.WriteLine($"Request version is {request.Version}\nHTTP Version is: {response11.Version}\n.NET Core Version: {System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription}");
+
+            Console.ReadKey();
+        }
+    }
+}
